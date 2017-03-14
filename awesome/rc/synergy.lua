16fef1e70d295e11821bed8f81f8bab7799027d9
Simon Désaulniers <sim.desaulniers@gmail.com>
2017-03-14 01:32:58 -0400

    awesome: fix synergy subprocess exit code bug
    On lua5.1, io.pipe doesn't return exit code. We have to use os.execute.
    
    This solution however includes FIXMEs and TODOs since a cleaner solution would
    be to use lua-spawn. However, managing moules like that with dependencies should
    be done with something like luarocks. I should consider analysing how it will
    integrate with my dotfiles. So this is TODO.

--- awesome/rc/synergy.lua
+++ awesome/rc/synergy.lua
@@ -1,9 +1,21 @@
 local naughty = require("naughty")
 
+require("rc.utils")
+
 -- Starts syenrgyc on synergyc_host and synergys on the this machine. This
 -- requires the remote host permits ssh acces to $USER with ssh key.
 function synergy(host)
+    package.path = package.path .. ';' .. config_dir .. '/penlight/lua/?.lua'
+    --TODO: use spawn.system(...) when luarocks is setup
+    -- package.path = package.path .. ';' .. config_dir .. '/lua-spawn/?/init.lua'
+    -- package.path = package.path .. ';' .. config_dir .. '/lua-spawn/?.lua'
+    -- package.cpath = package.cpath .. ';' .. config_dir .. '/lua-spawn/?.so'
+    require("pl.stringx").import()
+    --TODO: use spawn.system(...) when luarocks is setup
+    -- local spawn = require("spawn")
+
     host = host or 'ultralisk.local'
+    local notify_titletext = "Synergy"
 
     -- {{ little hack to get my ip address
     socket = require("socket")
@@ -12,28 +24,41 @@ function synergy(host)
     myip,_ = mysocket:getsockname()
     -- }}
 
+    -- resolve avahi hostname to ipv4 address
+    resolver = io.popen("avahi-resolve-host-name -4 " .. host)
+    host_ip = resolver:read("*l"):split('\t')[2]
+    resolver:close()
+
     --restarting synergyc on the remote host before starting synergy server.
-    synergy_call = 'ssh -o StrictHostKeyChecking=no ' ..  host ..
-        ' ' .. '"pkill -u $USER -x synergyc ; synergyc ' ..  myip .. '"'
-    j = 0
-    local synergied
-    repeat
-        synergied = {io.popen(synergy_call):close()}
-        j = j + 1
+    --TODO: use spawn.system(...) when luarocks is setup
+    local ret = os.execute('ssh -o StrictHostKeyChecking=no ' ..  host_ip ..  ' '
+                            .. '"pkill -u $USER -x synergyc ; synergyc ' ..  myip .. '"')
+    -- FIXME: Lua5.1 SUCKS. Use spawn.system(...) when luarocks is setup
+    local version = tonumber(_VERSION:split(' ')[2])
+    if version == 5.1 then
+        synergized = ret
+    elseif version > 5.1 then
+        synergized = ret[3]
+    else
+        naughty.notify({ preset = naughty.config.presets.critical,
+            title = notify_titletext,
+            text  = "What is this lua version? Get outta here!"
+        })
+    end
 
-        -- wait a second
-        t0 = os.clock()
-        while os.clock() - t0 <= 1 do end
-    until synergied[1] or j == 2 -- this let ssh/avahi a chance to resolve the host.
-
-    if synergied[1] then
-        notify_text = "Starting synergyc on " .. host .. "..."
+    naughty.notify({text = tostring(synergized)})
+    if synergized == 0 then
+        naughty.notify({
+            title = notify_titletext,
+            text  = "Starting synergyc on " .. host .. " (" .. host_ip .. ")" .. "..."
+        })
         run_once('synergys')
     else
-        notify_text = "Failed to connect to remote host \"" .. host .. "\"..."
+        naughty.notify({ preset = naughty.config.presets.critical,
+            title = notify_titletext,
+            text  = "Failed to start on remote host \"" .. host .. " (" .. host_ip .. ")" .. "\"... "
+                    .. "exit with code " .. tostring(synergized)
+        })
     end
-    naughty.notify({
-        title = "Synergy",
-        text  = notify_text
-    })
 end
+
