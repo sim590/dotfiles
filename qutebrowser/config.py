ac6cdfe05645723b73301020c625d336393258c9
Simon Désaulniers <sim.desaulniers@gmail.com>
2017-10-17 21:00:05 -0400

    qutebrowser: spellcheck, solve shadowed keys issue

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -17,6 +17,8 @@ c.url.searchengines = {
     "gh"     : "https://github.com/search?q={}"
 }
 
+c.spellcheck.languages = ["fr-FR", "en-CA"]
+
 ##################
 #  KEY BINDINGS  #
 ##################
@@ -25,26 +27,28 @@ c.url.searchengines = {
 ## passthrough allow ctrl-v for pasting clipboard
 config.unbind('<ctrl-v>', mode='passthrough')
 config.bind('<ctrl-z>','leave-mode', mode='passthrough')
-config.bind(';iy','hint images yank')
+config.bind(';iy', 'hint images yank')
 # sessions
-config.bind('sw', 'set-cmd-text -s :session-save --only-active-window')
-config.bind('s',  'session-save default')
+config.bind('sw',  'set-cmd-text -s :session-save --only-active-window')
+config.bind('ss',  'session-save default')
 # downloads
-config.bind('ed', 'download-open')
+config.bind('ed',  'download-open')
 # videos (mpv, castnow, youtube-dl)
-config.bind('x',  'spawn --userscript ~/bin/qutebrowser_bin/mpv')
-config.bind(';x', 'hint links spawn mpv --force-window --no-terminal --keep-open=yes --ytdl {hint-url}')
-config.bind('xc', 'spawn --userscript ~/bin/qutebrowser_bin/cast {url}')
-config.bind(';c', 'hint links spawn --userscript ~/bin/qutebrowser_bin/cast {hint-url}')
-config.bind('Yd', 'spawn youtube-dl {url}')
-config.bind(';yd','hint links spawn youtube-dl {hint-url}')
-config.bind('Ym', 'spawn youtube-dl --extract-audio --audio-format mp3 {url}')
-config.bind(';ym','hint links spawn youtube-dl --extract-audio --audio-format mp3 {hint-url}')
+config.bind('xv',  'spawn --userscript ~/bin/qutebrowser_bin/mpv')
+config.bind(';xv', 'hint links spawn mpv --force-window --no-terminal --keep-open=yes --ytdl {hint-url}')
+config.bind('xc',  'spawn --userscript ~/bin/qutebrowser_bin/cast {url}')
+config.bind(';xc', 'hint links spawn --userscript ~/bin/qutebrowser_bin/cast {hint-url}')
+## unbinding for preventing shadowing ;Yd and ;Ym
+config.unbind(';Y')
+config.bind('Yd',  'spawn youtube-dl {url}')
+config.bind(';Yd', 'hint links spawn youtube-dl {hint-url}')
+config.bind('Ym',  'spawn youtube-dl --extract-audio --audio-format mp3 {url}')
+config.bind(';Ym', 'hint links spawn youtube-dl --extract-audio --audio-format mp3 {hint-url}')
 # start/stop totally private browsing
-config.bind('gp', 'spawn --userscript ~/bin/qutebrowser_bin/totally private')
-config.bind('gP', 'spawn --userscript ~/bin/qutebrowser_bin/totally public')
+config.bind('gp',  'spawn --userscript ~/bin/qutebrowser_bin/totally private')
+config.bind('gP',  'spawn --userscript ~/bin/qutebrowser_bin/totally public')
 # password fill
-config.bind(';p', 'spawn --userscript /home/simon/bin/qutebrowser_bin/password_fill')
+config.bind(';p',  'spawn --userscript /home/simon/bin/qutebrowser_bin/password_fill')
 
 # vim:set et sw=4 ts=4 tw=120:
 
