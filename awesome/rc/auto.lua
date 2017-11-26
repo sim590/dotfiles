cfbcc0eb5a606c2f6be53065c29dfde4a20ab57f
Simon Désaulniers <sim.desaulniers@gmail.com>
2017-11-26 00:02:47 -0500

    awesome: autostart redshift (montreal)
    Latitude and longitude configuration for redshift are configured for Montreal,
    Québec.

--- awesome/rc/auto.lua
+++ awesome/rc/auto.lua
@@ -8,4 +8,5 @@ run_once("system-config-printer-applet")
 run_once("urxvtd")
 run_once("xrdb ~/.Xresources")
 run_once("xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55 &")
+run_once(redshift.." & disown")
 -- }}}
