6e5ca9bbf3ec327b39a4a819438be2fef1a77892
Simon Désaulniers <sim.desaulniers@gmail.com>
2018-03-18 11:16:22 -0400

    awesome/rules: proper indentation

--- awesome/rc/rules.lua
+++ awesome/rc/rules.lua
@@ -6,16 +6,16 @@ local beautiful = require("beautiful")
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     size_hints_honor = false,               -- closes the gaps in between temrinal windows.
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+        properties = { border_width = beautiful.border_width,
+            border_color     = beautiful.border_normal,
+            focus            = awful.client.focus.filter,
+            size_hints_honor = false,               -- closes the gaps in between temrinal windows.
+            raise            = true,
+            keys             = clientkeys,
+            buttons          = clientbuttons,
+            screen           = awful.screen.preferred,
+            placement        = awful.placement.no_overlap+awful.placement.no_offscreen
+        }
     },
 
     -- Floating clients.
@@ -50,22 +50,20 @@ awful.rules.rules = {
             "Alarm-clock-applet",
             "xtightvncviewer"},
 
-        name = {
-          "Event Tester",  -- xev.
-        },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
+            name = {
+                "Event Tester",  -- xev.
+            },
+            role = {
+                "AlarmWindow",  -- Thunderbird's calendar.
+                "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            }
+        }, properties = { floating = true }
+    },
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = false }
+        }, properties = { titlebars_enabled = false }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
 }
 -- }}}
