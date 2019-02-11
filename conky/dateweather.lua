1d4ad2ed805d6b49d2328f8ed8fd1f074756e98a
Simon Désaulniers <sim.desaulniers@gmail.com>
2019-02-10 22:52:26 -0500

    conky/weather: [+] variables for repeated strings

--- conky/dateweather.lua
+++ conky/dateweather.lua
@@ -1,3 +1,12 @@
+local weather_dir   = "~/.conky/dateweather"
+local imgs_dir      = weather_dir .. "/imgs"
+local parse_weather = weather_dir .. "/parse_weather"
+local get_weather   = weather_dir .. "/get_weather"
+
+local vision_cache_dir_path   = "~/.cache/conky-vision/"
+local weather_cache_path      = "~/.cache/conky-vision/weather.json"
+local forecast_cache_dir_path = "~/.cache/conky-vision/forecast"
+
 conky.config = {
     -------------------------------------
     --  Generic Settings
@@ -55,11 +64,11 @@ conky.config = {
     -------------------------------------
     --  Icon Sources
     -------------------------------------
-    template0='~/.conky/dateweather/imgs',  --  today
-    template1='~/.conky/dateweather/imgs',  --  +1day
-    template2='~/.conky/dateweather/imgs',  --  +2days
-    template3='~/.conky/dateweather/imgs',  --  +3days
-    template4='~/.conky/dateweather/imgs',  --  +4days
+    template0 = imgs_dir,  --  today
+    template1 = imgs_dir,  --  +1day
+    template2 = imgs_dir,  --  +2days
+    template3 = imgs_dir,  --  +3days
+    template4 = imgs_dir,  --  +4days
 
     -------------------------------------
     --  API Key
@@ -85,7 +94,7 @@ conky.config = {
 
 conky.text = [[
 \
-${execi 300 ~/.conky/dateweather/get_weather ${template6} ${template7} ${template8} ${template9}}\
+${execi 300 ]] .. get_weather .. [[ ${template6} ${template7} ${template8} ${template9}}\
 \
 ${font Poiret One:weight=Light:size=96}${color0}\
 ${alignc}${time %H:%M}\
@@ -98,22 +107,22 @@ ${font}${color}
 \
 ${font Poiret One:size=18}${color2}\
 ${voffset 36}\
-${goto 60}${execi 300 jq .main.temp ~/.cache/conky-vision/weather.json | awk '{print int($1+0.5)}' # round num}°\
+${goto 60}${execi 300 jq .main.temp ]] .. weather_cache_path .. [[ | awk '{print int($1+0.5)}' # round num}°\
 ${font}${color}\
 \
 ${font Poiret One:size=12}${color3}\
-${goto 164}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '1'}°\
-${goto 272}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '2'}°\
-${goto 378}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '3'}°\
-${goto 484}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '4'}°\
+${goto 164}${execi 300 ]] .. parse_weather .. [[ 'max' '.main.temp' '1'}°\
+${goto 272}${execi 300 ]] .. parse_weather .. [[ 'max' '.main.temp' '2'}°\
+${goto 378}${execi 300 ]] .. parse_weather .. [[ 'max' '.main.temp' '3'}°\
+${goto 484}${execi 300 ]] .. parse_weather .. [[ 'max' '.main.temp' '4'}°\
 ${font}${color}\
 \
 ${font Poiret One:size=12}${color4}\
 ${voffset 52}\
-${goto 218}${execi 300 ~/.conky/dateweather/parse_weather 'min' '.main.temp' '1'}°\
-${goto 324}${execi 300 ~/.conky/dateweather/parse_weather 'min' '.main.temp' '2'}°\
-${goto 430}${execi 300 ~/.conky/dateweather/parse_weather 'min' '.main.temp' '3'}°\
-${goto 536}${execi 300 ~/.conky/dateweather/parse_weather 'min' '.main.temp' '4'}°\
+${goto 218}${execi 300 ]] .. parse_weather .. [[ 'min' '.main.temp' '1'}°\
+${goto 324}${execi 300 ]] .. parse_weather .. [[ 'min' '.main.temp' '2'}°\
+${goto 430}${execi 300 ]] .. parse_weather .. [[ 'min' '.main.temp' '3'}°\
+${goto 536}${execi 300 ]] .. parse_weather .. [[ 'min' '.main.temp' '4'}°\
 ${font}${color}
 \
 ${font Poiret One:size=14}${color5}\
@@ -125,9 +134,17 @@ ${goto 394}${execi 300 LANG=${template9} LC_TIME=${template9} date -d +3days +%^
 ${goto 500}${execi 300 LANG=${template9} LC_TIME=${template9} date -d +4days +%^a}\
 ${font}${color}
 \
-${execi 300 cp -f ${template0}/$(jq .weather[0].id ~/.cache/conky-vision/weather.json).png ~/.cache/conky-vision/current.png}${image ~/.cache/conky-vision/current.png -p 72,266 -s 32x32}\
-${execi 300 cp -f ${template1}/$(~/.conky/dateweather/parse_weather 'first' '.weather[0].id' '1').png ~/.cache/conky-vision/forecast-1.png}${image ~/.cache/conky-vision/forecast-1.png -p 178,266 -s 32x32}\
-${execi 300 cp -f ${template2}/$(~/.conky/dateweather/parse_weather 'first' '.weather[0].id' '2').png ~/.cache/conky-vision/forecast-2.png}${image ~/.cache/conky-vision/forecast-2.png -p 284,266 -s 32x32}\
-${execi 300 cp -f ${template3}/$(~/.conky/dateweather/parse_weather 'first' '.weather[0].id' '3').png ~/.cache/conky-vision/forecast-3.png}${image ~/.cache/conky-vision/forecast-3.png -p 390,266 -s 32x32}\
-${execi 300 cp -f ${template4}/$(~/.conky/dateweather/parse_weather 'first' '.weather[0].id' '4').png ~/.cache/conky-vision/forecast-4.png}${image ~/.cache/conky-vision/forecast-4.png -p 496,266 -s 32x32}\
+${execi 300 cp -f ${template0}/$(jq .weather[0].id ]] .. weather_cache_path .. [[).png ]] ..
+    vision_cache_dir_path .. [[/current.png}${image ]] .. vision_cache_dir_path .. [[/current.png -p 72,266 -s 32x32}\
+${execi 300 cp -f ${template1}/$(]] .. parse_weather .. [[ 'first' '.weather[0].id' '1').png ]] ..
+    forecast_cache_dir_path .. [[-1.png}${image ]] .. forecast_cache_dir_path .. [[-1.png -p 178,266 -s 32x32}\
+${execi 300 cp -f ${template2}/$(]] .. parse_weather .. [[ 'first' '.weather[0].id' '2').png ]] ..
+    forecast_cache_dir_path .. [[-2.png}${image ]] .. forecast_cache_dir_path .. [[-2.png -p 284,266 -s 32x32}\
+${execi 300 cp -f ${template3}/$(]] .. parse_weather .. [[ 'first' '.weather[0].id' '3').png ]] ..
+    forecast_cache_dir_path .. [[-3.png}${image ]] .. forecast_cache_dir_path .. [[-3.png -p 390,266 -s 32x32}\
+${execi 300 cp -f ${template4}/$(]] .. parse_weather .. [[ 'first' '.weather[0].id' '4').png ]] ..
+    forecast_cache_dir_path .. [[-4.png}${image ]] .. forecast_cache_dir_path .. [[-4.png -p 496,266 -s 32x32}\
 ]]
+
+-- vim: set sts=4 ts=4 sw=4 tw=120 et :
+
