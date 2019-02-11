7013a83f99cc91925da0f3454290f1ede582edd1
Simon Désaulniers <sim.desaulniers@gmail.com>
2019-02-11 04:27:33 -0500

    conky: store main colors in config.lua

--- conky/config.lua
+++ conky/config.lua
@@ -1,6 +1,12 @@
 
 return {
-    wlan_interface = 'wlp4s0'
+    wlan_interface = 'wlp4s0',
+    white          = '#FFFFFF',
+    grey1          = '#DDDDDD',
+    grey2          = '#AAAAAA',
+    grey3          = '#888888',
+    orange         = '#EF5A29',
+    green          = '#77B753',
 }
 
 -- vim: set sts=4 ts=4 sw=4 tw=120 et :
