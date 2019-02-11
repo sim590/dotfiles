97b8a7dfd294950cc25f4e239a3ee164992a2ed0
Simon Désaulniers <sim.desaulniers@gmail.com>
2019-02-10 22:37:10 -0500

    conky/weather: fix indent and remove extra spaces

--- conky/dateweather.lua
+++ conky/dateweather.lua
@@ -1,135 +1,105 @@
 conky.config = {
+    -------------------------------------
+    --  Generic Settings
+    -------------------------------------
+    background=true,
+    update_interval=1,
+    double_buffer=true,
+    no_buffers=true,
+    imlib_cache_size=0,
 
--------------------------------------
---  Generic Settings
--------------------------------------
-background=true,
-update_interval=1,
-double_buffer=true,
-no_buffers=true,
-imlib_cache_size=0,
+    draw_shades=false,
+    draw_outline=false,
+    draw_borders=false,
 
-draw_shades=false,
-draw_outline=false,
-draw_borders=false,
+    -------------------------------------
+    --  Window Specifications
+    -------------------------------------
+    gap_x=0,
+    gap_y=0,
 
+    alignment="bl",
 
--------------------------------------
---  Window Specifications
--------------------------------------
-gap_x=0,
-gap_y=0,
+    minimum_height=400,
+    minimum_width=600,
 
-alignment="bl",
+    own_window=true,
+    own_window_type="override",
+    own_window_transparent=true,
+    own_window_hints="undecorated,below,sticky,skip_taskbar,skip_pager",
 
-minimum_height=400,
-minimum_width=600,
+    own_window_argb_visual=true,
+    own_window_argb_value=0,
 
-own_window=true,
-own_window_type="override",
-own_window_transparent=true,
-own_window_hints="undecorated,below,sticky,skip_taskbar,skip_pager",
+    -------------------------------------
+    --  Text Settings
+    -------------------------------------
+    use_xft=true,
+    xftalpha=1,
+    font="Droid Sans:size=10",
+    text_buffer_size=256,
+    override_utf8_locale=true,
 
-own_window_argb_visual=true,
-own_window_argb_value=0,
+    -------------------------------------
+    --  Color Scheme
+    -------------------------------------
+    default_color='FFFFFF',
 
+    color0='FFFFFF', -- clock
+    color1='FFFFFF', -- date
+    color2='FFFFFF', -- current temperature
+    color3='FFFFFF', -- high tempratures
+    color4='FFFFFF', -- low tempratures
+    color5='FFFFFF', -- days
 
--------------------------------------
---  Text Settings
--------------------------------------
-use_xft=true,
-xftalpha=1,
-font="Droid Sans:size=10",
-text_buffer_size=256,
-override_utf8_locale=true,
+    -------------------------------------
+    --  Icon Sources
+    -------------------------------------
+    template0='~/.conky/dateweather/imgs',  --  today
+    template1='~/.conky/dateweather/imgs',  --  +1day
+    template2='~/.conky/dateweather/imgs',  --  +2days
+    template3='~/.conky/dateweather/imgs',  --  +3days
+    template4='~/.conky/dateweather/imgs',  --  +4days
 
+    -------------------------------------
+    --  API Key
+    -------------------------------------
+    template6="679f2aabf32fb7e3e854b5a3192152ce",
 
--------------------------------------
---  Color Scheme
--------------------------------------
-default_color='FFFFFF',
+    -------------------------------------
+    --  City ID
+    -------------------------------------
+    template7="6077243",
 
-color0='FFFFFF', -- clock
-color1='FFFFFF', -- date
-color2='FFFFFF', -- current temperature
-color3='FFFFFF', -- high tempratures
-color4='FFFFFF', -- low tempratures
-color5='FFFFFF', -- days
+    -------------------------------------
+    --  Temp Unit (default, metric, imperial)
+    -------------------------------------
+    template8="metric",
 
-
--------------------------------------
---  Icon Sources
--------------------------------------
-template0='~/.conky/dateweather/imgs',  --  today
-template1='~/.conky/dateweather/imgs',  --  +1day
-template2='~/.conky/dateweather/imgs',  --  +2days
-template3='~/.conky/dateweather/imgs',  --  +3days
-template4='~/.conky/dateweather/imgs',  --  +4days
-
-
--------------------------------------
---  API Key
--------------------------------------
-template6="",
-
-
--------------------------------------
---  City ID
--------------------------------------
-template7="6077243",
-
-
--------------------------------------
---  Temp Unit (default, metric, imperial)
--------------------------------------
-template8="metric",
-
-
--------------------------------------
---  Locale (e.g. "es_ES.UTF-8")
---  Leave empty for default
--------------------------------------
-template9=""
-
+    -------------------------------------
+    --  Locale (e.g. "es_ES.UTF-8")
+    --  Leave empty for default
+    -------------------------------------
+    template9="fr_CA.UTF-8"
 }
-
-
----------------------------------------------------
----------------------------------------------------
-
 
 conky.text = [[
 \
-\
-\
-\
 ${execi 300 ~/.conky/dateweather/get_weather ${template6} ${template7} ${template8} ${template9}}\
-\
-\
-\
 \
 ${font Poiret One:weight=Light:size=96}${color0}\
 ${alignc}${time %H:%M}\
 ${font}${color}
-\
-\
-\
 \
 ${font Poiret One:weight=Light:size=28}${color1}\
 ${voffset 30}\
 ${alignc}${execi 300 LANG=${template9} LC_TIME=${template9} date +"%A, %B %d"}\
 ${font}${color}
 \
-\
-\
-\
 ${font Poiret One:size=18}${color2}\
 ${voffset 36}\
 ${goto 60}${execi 300 jq .main.temp ~/.cache/conky-vision/weather.json | awk '{print int($1+0.5)}' # round num}°\
 ${font}${color}\
-\
-\
-\
 \
 ${font Poiret One:size=12}${color3}\
 ${goto 164}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '1'}°\
@@ -137,9 +107,6 @@ ${goto 272}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '2'
 ${goto 378}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '3'}°\
 ${goto 484}${execi 300 ~/.conky/dateweather/parse_weather 'max' '.main.temp' '4'}°\
 ${font}${color}\
-\
-\
-\
 \
 ${font Poiret One:size=12}${color4}\
 ${voffset 52}\
@@ -149,9 +116,6 @@ ${goto 430}${execi 300 ~/.conky/dateweather/parse_weather 'min' '.main.temp' '3'
 ${goto 536}${execi 300 ~/.conky/dateweather/parse_weather 'min' '.main.temp' '4'}°\
 ${font}${color}
 \
-\
-\
-\
 ${font Poiret One:size=14}${color5}\
 ${voffset 20}\
 ${goto 76}${execi 300 LANG=${template9} LC_TIME=${template9} date +%^a}\
@@ -160,9 +124,6 @@ ${goto 288}${execi 300 LANG=${template9} LC_TIME=${template9} date -d +2days +%^
 ${goto 394}${execi 300 LANG=${template9} LC_TIME=${template9} date -d +3days +%^a}\
 ${goto 500}${execi 300 LANG=${template9} LC_TIME=${template9} date -d +4days +%^a}\
 ${font}${color}
-\
-\
-\
 \
 ${execi 300 cp -f ${template0}/$(jq .weather[0].id ~/.cache/conky-vision/weather.json).png ~/.cache/conky-vision/current.png}${image ~/.cache/conky-vision/current.png -p 72,266 -s 32x32}\
 ${execi 300 cp -f ${template1}/$(~/.conky/dateweather/parse_weather 'first' '.weather[0].id' '1').png ~/.cache/conky-vision/forecast-1.png}${image ~/.cache/conky-vision/forecast-1.png -p 178,266 -s 32x32}\
