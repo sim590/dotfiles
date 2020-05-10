fa9737e3910cc1d5840f13172086dcb622606027
Simon Désaulniers <sim.desaulniers@gmail.com>
2020-05-09 22:18:30 -0400

    ranger: [/] replace highlight with batcat

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -185,8 +185,9 @@ handle_mime() {
                 local pygmentize_format='terminal'
                 local highlight_format='ansi'
             fi
-            highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
-                --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}" && exit 5
+            batcat --style=numbers --color=always "${FILE_PATH}" && exit 5
+            # highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
+            #     --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}" && exit 5
             # pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}" -- "${FILE_PATH}" && exit 5
             exit 2;;
 
